4c1d1070accd8068871c643528ac1f1b3ee477f4
Isabella <isabella@isabella-OptiPlex-790.(none)>
2014-03-20 11:16:54 -0300

    Treinamento Hibernate

---
+++ src/br/com/caelum/financas/modelo/Conta.java
@@ -0,0 +1,65 @@
+package br.com.caelum.financas.modelo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "Conta_Corrente")
+@Entity
+public class Conta {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+
+	private String titular;
+	private String agencia;
+	private String numero;
+	private String banco;
+
+	@Column(name = "codigo")
+	private String codigoReferencia;
+
+	public String getTitular() {
+		return titular;
+	}
+
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getBanco() {
+		return banco;
+	}
+
+	public void setBanco(String banco) {
+		this.banco = banco;
+	}
+
+	public String getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(String agencia) {
+		this.agencia = agencia;
+	}
+
+	public String getCodigoReferencia() {
+		return codigoReferencia;
+	}
+
+	public void setCodigoReferencia(String codigoReferencia) {
+		this.codigoReferencia = codigoReferencia;
+	}
+
+}
---
+++ src/br/com/caelum/financas/teste/TesteInsereConta.java
@@ -0,0 +1,36 @@
+package br.com.caelum.financas.teste;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.caelum.financas.modelo.Conta;
+
+public class TesteInsereConta {
+
+	public static void main(String[] args) {
+		long inicio = System.currentTimeMillis();
+
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("controlefinancas");
+		EntityManager entityManager = factory.createEntityManager();
+
+		Conta conta = new Conta();
+		conta.setTitular("José Roberto");
+		conta.setBanco("Banco do Brasil");
+		conta.setNumero("123456-6");
+		conta.setAgencia("0999");
+		conta.setCodigoReferencia("798");
+
+		entityManager.getTransaction().begin();
+		entityManager.persist(conta);
+		entityManager.getTransaction().commit();
+		entityManager.close();
+
+		System.out.println("Conta gravada com sucesso!");
+
+		long fim = System.currentTimeMillis();
+		System.out.println("Executado em: " + (fim - inicio));
+
+	}
+
+}
